9507fdc2cc9b52961f1afce7fd477757c7019816
ShadowsAdi <54354938+ShadowsAdi@users.noreply.github.com>
2022-10-27 19:02:51 +0300

    Update numere_romana.cpp

--- numere_romana.cpp
+++ numere_romana.cpp
@@ -20,30 +20,30 @@ int main()
 
 	int len = strlen(sString);
 
-    sArray['i', 'I'] = 1;
-    sArray['v', 'V'] = 5;
-    sArray['x', 'X'] = 10;
-    sArray['l', 'L'] = 50;
-    sArray['c', 'C'] = 100;
-    sArray['D', 'd'] = 500;
-    sArray['M', 'm'] = 1000;
+	sArray['i', 'I'] = 1;
+	sArray['v', 'V'] = 5;
+	sArray['x', 'X'] = 10;
+	sArray['l', 'L'] = 50;
+	sArray['c', 'C'] = 100;
+	sArray['D', 'd'] = 500;
+	sArray['M', 'm'] = 1000;
 
-    // XCIX
+	// XCIX
 
-    int iPre = 0, iPost, iIterator = 0;
-    int i = len - 1;
-    do
-    {
-        iPost = sArray[sString[i]];
-        
-        iIterator = iPost >= iPre ? 1 : -1;
+	int iPre = 0, iPost, iIterator = 0;
+	int i = len - 1;
+	do
+	{
+		iPost = sArray[sString[i]];
 
-        iNumar += iIterator * iPost;
-        iPre = iPost;
-        i--;
-    } 
-    while(i >= 0);
-	
+		iIterator = iPost >= iPre ? 1 : -1;
+
+		iNumar += iIterator * iPost;
+		iPre = iPost;
+		i--;
+	} 
+	while(i >= 0);
+
 	printf("Numar roman: %s \nNumar decimal: %d\n", sString, iNumar);
 
 	return 0;
